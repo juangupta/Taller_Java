6fe249dc451976cd5b93472dbe75409ef2e7d779
juagomez <juagomez@pb0b0560385.bancolombia.corp>
2017-09-04 10:46:16 -0500

    Commit Final

--- src/AppMain.java
+++ src/AppMain.java
@@ -4,8 +4,11 @@ public class AppMain {
 	public static void main(String[] args) {
 		
 		Calculadora miCalculadora = new Calculadora();
-		int[] sumandos = {1,5,9,10};
-		float suma = miCalculadora.suma(sumandos);
+		
+		//SUMA
+		float[] sumandos = {1,5,9,10};
+		float suma = miCalculadora.sumar(sumandos);
+		System.out.println("\nSuma");
 		for (int i = 0; i < sumandos.length; i++)
 		{
 			if (i == sumandos.length-1)
@@ -21,6 +24,69 @@ public class AppMain {
 		}
 		
 		System.out.println("=" + suma);
+		
+		//RESTA
+		
+		float minuendo = 30;
+		float[] sustraendos = {1,5,9,10};
+		float resta = miCalculadora.restar(minuendo, sustraendos);
+		System.out.println("\nResta");
+		System.out.println(minuendo);
+		for (int i = 0; i < sustraendos.length; i++)
+		{
+				System.out.println("-" + sustraendos[i]);
+
+			
+		}
+		
+		System.out.println("=" + resta);
+		
+		//MULTIPLICACION
+		
+		float[] factores = {1,5,9,10};
+		float producto = miCalculadora.multiplicar(factores);
+		System.out.println("\nMultiplicacion");
+		for (int i = 0; i < factores.length; i++)
+		{
+			if (i == factores.length-1)
+			{
+				System.out.println(factores[i]);				
+			}
+			else
+			{
+				System.out.println(factores[i] + "X");
+			}
+
+			
+		}
+		
+		System.out.println("=" + producto);
+		
+		//DIVISION
+		
+				float dividendo = 30;
+				float[] divisores = {2,5};
+				float cociente;
+				try
+				{
+					cociente = miCalculadora.dividir(dividendo, divisores);	
+				}
+				catch (Exception e)
+				{
+					System.out.println(e);
+					return;
+				}
+				System.out.println("\nDivision");
+				System.out.println(dividendo);
+				for (int i = 0; i < divisores.length; i++)
+				{
+						System.out.println("/" + divisores[i]);
+
+					
+				}
+				
+				System.out.println("=" + cociente);
+		
 	}
 
 }
--- src/Calculadora.java
+++ src/Calculadora.java
@@ -7,7 +7,7 @@ public class Calculadora
 		
 	}
 	
-	public float suma(int sumandos[])
+	public float sumar(float sumandos[])
 	{
 		float total = 0;
 		for (int i = 0; i < sumandos.length; i++)
@@ -15,6 +15,47 @@ public class Calculadora
 			total = total + sumandos[i];
 		}
 		return total;
+	}
+	
+	public float restar (float minuendo, float sustraendos[])
+	{
+		float resto = minuendo;
+		for (int i = 0; i < sustraendos.length; i++)
+		{
+			resto = resto - sustraendos[i];
+		}
+		
+		return resto;
+	}
+	
+	public float multiplicar (float factores[])
+	{
+		float producto = 1;
+		for (int i = 0; i < factores.length; i++)
+		{
+			producto = producto * factores[i];
+		}
+		
+		return producto;
+	}
+	
+	public float dividir (float dividendo, float divisores[])
+	{
+		float cociente = dividendo;
+		for (int i = 0; i < divisores.length; i++)
+		{
+			if (divisores[i] != 0)
+			{
+				cociente = cociente / divisores[i];	
+			}
+			else
+			{
+				throw new ArithmeticException("No se permite divisores iguales a cero");
+			}
+			
+		}
+		
+		return cociente;
 	}
 
 }
